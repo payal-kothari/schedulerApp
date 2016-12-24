01ee3c3820f8bd98d158a29f13c41c278aa47b7a
payal kothari <pylkothari@gmail.com>
2016-12-23 19:28:35 -0500

    till analog time picker with new activity

--- app/src/main/java/listviewfromsqlitedb/example/com/scheduler/MainActivity.java
+++ app/src/main/java/listviewfromsqlitedb/example/com/scheduler/MainActivity.java
@@ -6,14 +6,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
 public class MainActivity extends Activity {
